ec30fe44fd8e8e28753749b77b777e21f12ca59a
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-01-17 15:37:04 -0500

    firts commit

---
+++ src/components/pure/greeting.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class Greeting extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            age: 23
+        }
+        
+    }
+
+
+
+    render() {
+        return (
+            <div>
+
+            <h1>
+                HOLA {this.props.name}!
+            </h1>
+
+            <h2>
+                Tu edad es de: {this.state.age}
+            </h2>
+                
+            </div>
+        );
+    }
+}
+
+
+Greeting.propTypes = {
+
+    name: PropTypes.string,
+};
+
+
+export default Greeting;
