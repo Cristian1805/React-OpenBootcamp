2e5d9916d47689c3e1b0011619d093ccf8d27890
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-01-25 12:18:17 -0500

    sesion 3 terminada, faltan visualizar algunos reultados en el host

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 //import funcion_greeting from './components/pure/funcion_greeting';
-import Funcion_greeting from './components/pure/funcion_greeting';
+//import Funcion_greeting from './components/pure/funcion_greeting';
+import TaskListComponent from './components/containers/task_list';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
         {/* Componente propio Greeting.js*/ }
         {/* <Greeting></Greeting> */}
         {/* <funcion_greeting name ="Cristhian"></funcion_greeting> */}
+        {/* <funcion_greeting name="Cristhian"></funcion_greeting> */}
 
-        <funcion_greeting name="Cristhian"></funcion_greeting>
+        <TaskListComponent></TaskListComponent>
 
         
       </header>
