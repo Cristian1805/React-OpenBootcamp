71b3636ba5cd3dbde6b27cf802e1383f2d5670a2
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-02-14 11:07:17 -0500

    Taream Realizada

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 //Añadirmos bootstrap a nuestro proyecto
 import 'bootstrap/dist/css/bootstrap.css';
-//!Los estilos propioos deben ir debajo del bootstrap para no pisarlos
+//!Los estilos propios deben ir debajo del bootstrap para no pisarlos
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+ s
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
